5a3fc6d6153d6e1016adf5dd1cfd5249cf8225b6
Sumit Kumar <sumitgupta0675@gmail.com>
2026-03-30 14:55:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,18 +1,16 @@
 class Solution {
-    public int solve(int i, int cost[], int dp[]) {
-        if(i == 0 ||i == 1 ) return cost[i];
-        //if(i == 1) return cost[1];
-
-        if(dp[i] != -1) return dp[i];
-
-        return dp[i] = cost[i] + Math.min(solve(i-1,cost,dp),solve(i-2,cost,dp));
-    }
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int[] dp = new int[n];
 
-        Arrays.fill(dp,-1);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
 
-        return Math.min(solve(n-1,cost,dp),solve(n-2,cost,dp));
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+        }
+
+        return Math.min(dp[n - 1], dp[n - 2]);
+        
     }
 }
