468067b6b05faa12c2ec231fc517e56926fecbb4
Sumit Kumar <sumitgupta0675@gmail.com>
2026-04-06 14:07:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,7 +2,7 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
 
-        if(n < 2) return nums[0];
+    //    if(n < 2) return nums[0];
 
         int[] dp = new int[n];
 
