46b7ac6652b5e5837d44f3b005f12cc8ce2b3a53
Sumit Kumar <sumitgupta0675@gmail.com>
2026-04-15 10:48:45 +0530

    Time: 2 ms (96.49%), Space: 48.5 MB (89.01%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] countBits(int n) {
+
+        int result[] = new int[n + 1];
+
+        for(int i = 0; i <= n ;i++){
+            result[i] = countOnes(i);
+        }
+        return result;
+    }
+    public int countOnes(int num){
+        int count= 0;
+
+        while(num > 0){
+            num = num & (num - 1);
+            count++;
+        }
+        return count;
+    }
+}
