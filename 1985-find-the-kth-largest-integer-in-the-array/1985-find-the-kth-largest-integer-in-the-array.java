48cca5e305709ba338069e243610a05f3dd71321
Sumit Kumar <sumitgupta0675@gmail.com>
2026-06-04 14:46:50 +0530

    Time: 22 ms (61.7%), Space: 51.3 MB (82%) - LeetHub

---
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String kthLargestNumber(String[] nums, int k) {
+        
+        PriorityQueue<String> pq = new PriorityQueue<>((a,b) -> {
+            if(a.length() != b.length()){
+                return a.length() - b.length();
+            }
+            return a.compareTo(b);
+        });
+        for(int i = 0;i < nums.length; i++){
+            pq.add(nums[i]);
+
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
